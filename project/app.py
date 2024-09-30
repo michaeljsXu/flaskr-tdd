65fb2924ca95d1ce353c72f2445660d3ccfc1d73
Michael Xu <michaelx.0277@gmail.com>
2024-09-30 19:34:50 -0400

    added login.html template and added more imports

--- project/app.py
+++ project/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import sqlite3
-from flask import Flask, g, render_template
+from flask import Flask, g, render_template, request, session, flash, redirect, url_for
 import os
 from . import config
 # configuration
